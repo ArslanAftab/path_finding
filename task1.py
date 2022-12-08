f7259d5a78c4d846c489a2d60ab89cfe72ddd95b
ArslanAftab-LambertLabs <arslan.aftab@lambertlabs.com>
2022-12-08 19:39:08 +0000

    task 1 and 2 implemented

--- task1.py
+++ task1.py
@@ -1,45 +1,88 @@
-import heapq # used to implement front
+import heapq # Used to implement front
+import math # Used to calculate sqrt(2)
 
 #  Dijkstra’s Algorithm on Nodes and Edges
-
-# Represent map as 2d array, i.e. 
-# [ Node 1 costs,
-#   Node 2 costs, 
-#   Node 3 costs,
-#  ...] 
-
 inf = float('inf')
+r2 = math.sqrt(2)
 
-map = [
-    [0, 1, inf, 1, inf],
-    [1, 0, 4, 1, inf],
-    [inf, 4, 0, 1, 4],
-    [1, 1, 1, 0, 2],
-    [inf, inf, 4, 2, 0]
-]
-
-def djikstra(map, start, end):
+def djikstra(graph, start, end):
     visited = set()
+    pathway = {}
+    distance = {i+1: inf for i in range(len(graph))}
     front = []
-    
-    heapq.heappush(front, (0, start, start))
-
+    heapq.heappush(front, (0, start, None))
+    print(distance)
     while front:
-        # Consider node, n, in front with lowest cost
-        _, node, predecessor = heapq.heappop(front)
+        # Choose node, n, with lowest cost
+        cost, node, predecessor = heapq.heappop(front)
         
-        # Move n to visited
-        visited.add(node)
-        
-        for neighbourNode, cost in enumerate(map[node]):
-            if neighbourNode in visited:
+        if node in visited:
                 continue # Unvisited neighbours
 
-            print(f'{node}|{neighbourNode} at: {cost}')
+        # Add to visited set
+        visited.add((cost, node, predecessor))
 
+        print(f"I'm at node {node}")
+        for neighbour in graph[node]:
+            # Look at all unvisited neighbours
+            if neighbour in visited:
+                continue
+
+            adjustedCost = cost + graph[node][neighbour]
             
-            heapq.heappush(front, (cost, neighbourNode, node))
+            print(f'{node} -> {neighbour}: {adjustedCost}')
 
-        print(f'Visited: {visited}')
+            # Adjust cost when new cost is lower
+            if adjustedCost < distance[neighbour]:
+                distance[neighbour] = adjustedCost
+                pathway[neighbour] = node
+                heapq.heappush(front, (adjustedCost, neighbour, node))
+
         print(f'Front: {front}')
-djikstra(map, 0, 5)
+        print(f'Visited: {visited}')
+
+    print(f'\n\nShortest distances from {start}: {distance}')
+    
+    # Visualise pathway
+
+    steps = [end]
+    current = end
+    while current != start:
+        current = pathway[current]
+        steps.append(current)
+    steps.reverse()
+
+    print(f'To get from: {start} -> {end}\nVisit:{steps}')
+
+
+# Map implementation
+# i.e. graph[1][2] gives cost node 1 -> node 2
+
+graph_task_1 = {
+    1: {1:0, 2:1, 4:1},
+    2: {2:0, 1:1, 3:4, 4:1},
+    3: {3:0, 2:4, 4:1, 5:4},
+    4: {4:0, 1:1, 2:1, 3:1, 5:2},
+    5: {5:0, 3:4, 4:2}
+}
+
+graph_task_2 = {
+    1: {1:0, 2:1, 5:1},
+    2: {2:0, 1:1, 5:r2, 7:r2, 3:1},
+    3: {3:0, 2:1, 7:1, 8:r2, 4:1},
+    4: {4:0, 3:1, 7:r2, 8:1},
+    5: {5:0, 9:1, 2:r2, 1:1},
+    6: {6:0},
+    7: {7:0, 11:1, 8:1, 4:r2, 3:1, 2:r2},
+    8: {8:0, 7:1, 11:r2, 4:1, 3:r2},
+    9: {9:0, 13:1, 14:r2, 5:1},
+    10: {10:0},
+    11: {11:0, 14:r2, 15:1, 16:r2, 8:r2, 7:1},
+    12: {12:0},
+    13: {13:0, 14:1, 9:1},
+    14: {14:0, 13:1, 15:1, 11:r2},
+    15: {15:0, 14:1, 16:1, 11:1},
+    16: {16:0, 15:1, 11:r2},
+}
+
+djikstra(graph_task_2, 1, 16)
--- test.py
+++ test.py
@@ -6,9 +6,9 @@ inf = float('inf')
 def djikstra(graph, start, end):
     visited = set()
     pathway = {}
-    distance = {i+1: inf for i in range(len(graph))}
+    distance = {i: inf for i in range(len(graph))}
     front = []
-    heapq.heappush(front, (0, start, start))
+    heapq.heappush(front, (0, start, None))
 
     while front:
         # Choose node, n, with lowest cost
@@ -20,15 +20,17 @@ def djikstra(graph, start, end):
         # Add to visited set
         visited.add((cost, node, predecessor))
 
-        for neighbour in graph[node]:
+        print(f"I'm at node {node}")
+        for neighbour, difference in enumerate(graph[node]):
             # Look at all unvisited neighbours
             if neighbour in visited:
                 continue
 
+            print(f'DEBUG: neighbour:{neighbour} difference:{difference}')
             adjustedCost = cost + graph[node][neighbour]
-            
             print(f'{node} -> {neighbour}: {adjustedCost}')
             
+
             # Adjust cost when new cost is lower
             if adjustedCost < distance[neighbour]:
                 distance[neighbour] = adjustedCost
@@ -55,12 +57,13 @@ def djikstra(graph, start, end):
 # Map implementation
 # i.e. graph[1][2] gives cost node 1 -> node 2
 
-graph = {
-    1: {1:0, 2:1, 4:1},
-    2: {2:0, 1:1, 3:4, 4:1},
-    3: {3:0, 2:4, 4:1, 5:4},
-    4: {4:0, 1:1, 2:1, 3:1, 5:2},
-    5: {5:0, 3:4, 4:2}
-}
+task1_graph = [
+    [0, 1, inf, 1, inf],
+    [1, 0, 4, 1, inf],
+    [inf, 4, 0, 1, 4],
+    [1, 1, 1, 0, 2],
+    [inf, inf, 4, 2, 0]
+]
 
-djikstra(graph, 1, 5)
+
+djikstra(task1_graph, 0, 4)
